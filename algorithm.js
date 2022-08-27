3c291715b555811671174e74e470607122a4270e
ssunip <ssunip99@gmail.com>
2022-08-27 16:45:24 +0900

    initial commit

---
+++ algorithm.js
@@ -0,0 +1,16 @@
+let input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "example.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+// 입력 데이터 정제과정. fs모듈 사용하여 txt 파일 읽어오고, 문자로 변환
+
+input = input[0];
+input = input.split(" ").map(Number);
+
+solution(input[0], input[1]);
+
+function solution(a, b) {
+  console.log();
+}
