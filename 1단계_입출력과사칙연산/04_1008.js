16b988fab8c815d497c992d679ebd0b33d8fbb14
ssunip <ssunip99@gmail.com>
2022-08-29 23:48:06 +0900

    백준 1008번 완료

---
+++ 1단계_입출력과사칙연산/04_1008.js
@@ -0,0 +1,14 @@
+let input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "example.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+input = input[0];
+input = input.split(" ").map(Number);
+
+solution(input[0], input[1]);
+
+function solution(a, b) {
+  console.log(a / b);
+}
