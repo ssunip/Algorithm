d2afe52fa269ca632060eeeb1b4c8b19786a6e55
송인선 <104997140+ssunip@users.noreply.github.com>
2022-09-11 00:25:53 +0900

    [Bronze V] Title: A+B - 5, Time: 136 ms, Memory: 9572 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,17 @@
+let input = require("fs")
+  .readFileSync(process.platform === "linux" ? 0 : "example.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+solution(input);
+
+function solution(arr) {
+  let newArr = [];
+  let sum = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    newArr = arr[i].split(" ").map(Number);
+    sum = newArr[0] + newArr[1];
+    console.log(sum);
+  }
+}
