9a12b3216cec2ecccb27093e9f8faa01cabb9838
송인선 <104997140+ssunip@users.noreply.github.com>
2022-11-09 20:40:38 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 3.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -3,6 +3,7 @@ function solution(n) {
     let splited = String(n).split("")
     // 큰 수부터 작은 수로 정렬하기
     let sorted = splited.map(Number).sort((a,b) => b-a)
+    console.log(sorted, typeof sorted[0])
     // 다시 수로 만들어 리턴
     let result = ""
     for(let i = 0; i < sorted.length; i++) {
