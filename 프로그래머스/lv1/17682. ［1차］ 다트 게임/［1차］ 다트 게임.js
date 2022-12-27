dbf9395e9ae25a5180bc61c886b4e00ccbaab353
송인선 <104997140+ssunip@users.noreply.github.com>
2022-12-28 04:52:53 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,30 @@
+function solution(dartResult) {
+    let result = []
+    dartResult = dartResult.split("")
+    
+    for(let i = 0; i < dartResult.length; i++) {
+        if(!isNaN(dartResult[i])) {
+        dartResult[i] = (dartResult[i-1] === "1" ? 10 : dartResult[i]);
+        }
+        
+        if(dartResult[i] === "S"){
+            result.push(Math.pow(dartResult[i-1],1))
+        }
+        if(dartResult[i] === "D"){
+            result.push(Math.pow(dartResult[i-1],2))
+        }
+        if(dartResult[i] === "T"){
+            result.push(Math.pow(dartResult[i-1],3))
+        }
+        
+        if(dartResult[i] === "*"){
+            result[result.length-2] = result[result.length-2]*2
+            result[result.length-1] = result[result.length-1]*2
+        }
+            
+        if(dartResult[i] === "#"){
+            result[result.length-1] = result[result.length-1]*-1
+        }
+    }
+    return result.reduce((acc, cur) => acc+cur)
+}
